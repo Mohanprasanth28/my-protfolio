fe818696c93fee4acfbd2be47e062ca05c6c2102
Mohanprasanth28 <mohan28mass@gmail.com>
2024-12-24 21:14:15 +0530

    contact-update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,9 +6,8 @@ const Contact = () => {
       <h2>Contact Me</h2>
       <p>Email: mohan28mass@gmail.com</p>
       <p>LinkedIn: <a href="https://www.linkedin.com/in/mohan-prasanth-909b79255">Mohan Prasanth</a></p>
-      <p>GitHub: <a href="https://github.com/your-username">Your GitHub</a></p>
+      <p>GitHub: <a href="https://github.com/Mohanprasanth28">Your GitHub</a></p>
     </div>
   );
 };
-
 export default Contact;
