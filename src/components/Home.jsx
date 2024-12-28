92805c7e2476a218078bb69e11f172f24fd711e2
Mohanprasanth28 <mohan28mass@gmail.com>
2024-12-28 21:18:44 +0530

    add image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import mohanImage from '.././assets/mohan.jpg';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>
+    <div class ="class">
       <h1>Welcome to My Portfolio</h1>
       <p>I'm Mohan Prasanth, a Computer Science student and developer.</p>
+      <img src={mohanImage} alt="Mohan's Picture" />
+
     </div>
   );
 };
